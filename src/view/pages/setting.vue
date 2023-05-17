11a9f9eb9b2e41adc54affdd80cfd736b7ed5b60
上善若水 <119553246+ssrsyds@users.noreply.github.com>
2023-05-17 14:32:30 +0800

    Update setting.vue

--- src/view/pages/setting.vue
+++ src/view/pages/setting.vue
@@ -4,7 +4,7 @@
       <el-header>
         <transition name="el-zoom-in-top">
           <div v-show="show" class="transition-box">
-            <h3>JUN CHEN MO</h3>
+            <h3>AI百晓生-附属版</h3>
           </div>
         </transition>
       </el-header>
@@ -12,18 +12,17 @@
         <transition name="el-zoom-in-top">
           <div v-show="show" class="transition-box">
             <span>
-              现已接入OpenAI的Models API、Completions API、Chat API、Audio API、Images API、Files API、Fine-tunes API后续会添加更多有意思的功能进去，希望大家给我的GitHub点个小小的星星，
-              大家如果有什么好的想法可以在GitHub中提出来，觉得项目不错的朋友可以打赏一波，感谢支持。
+              一站式人工智能AI工具，润色文章、修正语法、生成文案、写代码、聊天，绘图，全方位提升网页效率！
             </span>
             <div>
-              <a href="https://space.bilibili.com/326625155?spm_id_from=333.337.0.0">BliBili</a>---<a href="https://github.com/202252197/ChatGPT_JCM">GitHub</a>
+              <a href="http://wj.lzlspyxgs.asia/">轻工具资源站</a>
             </div>
             <div>
               <h3>愿半生编码，如一生老友</h3>
             </div>
             <div>
-              <img src="https://i.328888.xyz/2023/05/09/iQ8tvv.jpeg" width="300px" height="300px"/>
-              <img src="https://i.328888.xyz/2023/05/09/iQNnf3.jpeg" width="300px" height="300px"/>
+              <img src="http://aai.yjie.fun/ds//img/alipay.png" width="300px" height="300px"/>
+              <img src="http://aai.yjie.fun/ds//img/wxpay.png" width="300px" height="300px"/>
             </div>
           </div>
         </transition>
